34406bf4b23fee0e92165e8f99d8d2b989b3d7bf
Gabriel Sevilha <58699454+gabrielsevilha@users.noreply.github.com>
2021-07-13 12:40:24 -0300

    Add files via upload

--- fps_count.h
+++ fps_count.h
@@ -1,18 +1,18 @@
 #include<stdio.h>
 #include<time.h>
 
-double current_time = 0;
-double last_time = 0;
-int fps = 0;
+double fps_current_time = 0;
+double fps_last_time = 0;
+int fps_fps = 0;
 
 void printfps(){
 	
-	fps++;
-	current_time = time(0);
-	if(current_time - last_time > 0){
-		printf("fps: %d\n",fps);
-		last_time = current_time;
-		fps = 0;
+	fps_fps++;
+	fps_current_time = time(0);
+	if(fps_current_time - fps_last_time > 0){
+		printf("fps: %d\n",fps_fps);
+		fps_last_time = fps_current_time;
+		fps_fps = 0;
 	}
 	
 }
@@ -21,12 +21,12 @@ int getfps(){
 
 	static int last_fps = 0;
 	
-	fps++;
-	current_time = time(0);
-	if(current_time - last_time > 0){
-		last_time = current_time;
-		last_fps = fps;
-		fps = 0;
+	fps_fps++;
+	fps_current_time = time(0);
+	if(fps_current_time - fps_last_time > 0){
+		fps_last_time = fps_current_time;
+		last_fps = fps_fps;
+		fps_fps = 0;
 	}
 	
 	return last_fps;
